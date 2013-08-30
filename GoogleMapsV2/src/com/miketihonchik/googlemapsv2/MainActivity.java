afadc379652e4653f63629a5622a8c4f2d2c8e4d
miketihonchik <mike.tihonchik@gmail.com>
2013-08-30 14:17:41 -0500

    add markers for location; center/zoom/animate camera upadate to those markers

--- GoogleMapsV2/src/com/miketihonchik/googlemapsv2/MainActivity.java
+++ GoogleMapsV2/src/com/miketihonchik/googlemapsv2/MainActivity.java
@@ -1,27 +1,44 @@
 package com.miketihonchik.googlemapsv2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.miketihonchik.googlemapsv2.app.AppDefines;
 
 public class MainActivity extends Activity {
 	private GoogleMap googleMap;
+	private List<Marker> markerList = new ArrayList<Marker>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		try {
-			initilizeMap();
+			initialize();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void initilizeMap() {
+	private void initialize() {
+		initializeMap();
+		initializeDefaultLocation();
+	}
+
+	private void initializeMap() {
 		if (googleMap == null) {
 			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
 					R.id.map)).getMap();
@@ -34,9 +51,36 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	private void initializeDefaultLocation() {
+		googleMap.addMarker(new MarkerOptions()
+				.position(AppDefines.VENTSPILS)
+				.title("Ventpils")
+				.snippet("Ventspils, Latvia")
+				.icon(BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+		Marker marker = googleMap.addMarker(new MarkerOptions()
+				.position(AppDefines.KULDIGAS23)
+				.title("Kuldīgas iela 23")
+				.snippet("Ventspils, Latvia")
+				.icon(BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+		marker.showInfoWindow();
+		centerZoomMap();
+	}
+
+	private void centerZoomMap() {
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(AppDefines.KULDIGAS23) // center of the map
+				.zoom(14).bearing(0) // orientation of the camera to east
+				.tilt(30) // tilt of the camera
+				.build();
+		googleMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPosition));
+	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		initilizeMap();
+		initialize();
 	}
 }
---
+++ GoogleMapsV2/src/com/miketihonchik/googlemapsv2/app/AppDefines.java
@@ -0,0 +1,8 @@
+package com.miketihonchik.googlemapsv2.app;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class AppDefines {
+	public static final LatLng VENTSPILS = new LatLng(57.404653,21.565368);
+	public static final LatLng KULDIGAS23 = new LatLng(57.392976,21.564482);
+}
